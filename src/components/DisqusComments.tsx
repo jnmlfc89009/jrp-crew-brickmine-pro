6f3153abf15dd86e3080e7eac74244e6e0426661
AI Studio Agent <jnmlfc89009@gmail.com>
2026-08-06 05:22:58 +0000

    Fix Disqus script error by using disqus-react component

--- src/components/DisqusComments.tsx
+++ src/components/DisqusComments.tsx
@@ -1,41 +1,16 @@
-import { useEffect } from 'react';
+import { DiscussionEmbed } from 'disqus-react';
 
 export function DisqusComments() {
-  useEffect(() => {
-    const disqusEmbedId = 'disqus-embed-script';
-    const disqusCountId = 'dsq-count-scr';
-    const d = document;
-
-    // Optional: Setup Disqus config before loading the script
-    (window as any).disqus_config = function () {
-      this.page.url = window.location.href;
-      this.page.identifier = window.location.pathname;
-    };
-
-    if (!d.getElementById(disqusEmbedId)) {
-      const s = d.createElement('script');
-      s.id = disqusEmbedId;
-      s.src = 'https://jrp-crew-brickmine.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', new Date().getTime().toString());
-      (d.head || d.body).appendChild(s);
-    }
-
-    if (!d.getElementById(disqusCountId)) {
-      const countScript = d.createElement('script');
-      countScript.id = disqusCountId;
-      countScript.src = 'https://jrp-crew-brickmine.disqus.com/count.js';
-      countScript.async = true;
-      (d.head || d.body).appendChild(countScript);
-    }
-  }, []);
-
   return (
     <div className="w-full max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 mt-16 mb-8">
-      <div id="disqus_thread"></div>
-      <noscript>
-        Please enable JavaScript to view the{' '}
-        <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
-      </noscript>
+      <DiscussionEmbed
+        shortname="jrp-crew-brickmine"
+        config={{
+          url: window.location.href,
+          identifier: window.location.pathname,
+          title: "JRP Crew - Brickmine",
+        }}
+      />
     </div>
   );
 }
